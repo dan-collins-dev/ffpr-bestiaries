342da06f1ae90b7277217cb3269ad5ed72052c14
Dan Collins <dan.collins.dev@gmail.com>
2024-08-23 19:41:42 -0400

    routing for ff1 content updated

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script>
+	import '../app.css';
+</script>
+
+
+<header>
+	<p>FFPR Compendium</p>
+	<nav>
+		<ul>
+			<li><a href="/">Home</a></li>
+	</nav>
+</header>
+
+
+<style>
+	header {
+		background-color: blue;
+		color: white;
+		display: flex;
+		padding: 1rem .5rem;
+	}
+
+    ul {
+        display: flex;
+    }
+	
+	p {
+		font-size: 1rem;
+        padding: .5rem;
+	}
+
+	nav {
+		margin-left: auto;
+	}
+
+	li {
+		list-style: none;
+        padding: .5rem;
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+        
+	}
+</style>
+
+<slot />
