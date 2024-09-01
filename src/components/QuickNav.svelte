9c3a96209d306bf6fb7f0df5185bb0646505e798
Dan Collins <dan.collins.dev@gmail.com>
2024-09-01 15:35:46 -0400

    Added a quicker way to switch between log contexts

---
+++ src/components/QuickNav.svelte
@@ -0,0 +1,20 @@
+<script>
+import { base } from '$app/paths';
+</script>
+
+<div>
+    <a href="{base}/ff1/bestiary">Bestiary</a>
+    <a href="{base}/ff1/chests">Chest Log</a>
+</div>
+
+<style>
+    div {
+        display: flex;
+        padding: 1rem;
+    }
+
+    a {
+        padding: 1rem;
+        flex-grow: 1;
+    }
+</style>
--- src/routes/ff1/bestiary/+page.svelte
+++ src/routes/ff1/bestiary/+page.svelte
@@ -2,8 +2,10 @@
 	import { ff1Bestiary } from '$store';
 	import EncounterTotal from '$components/EncounterTotal.svelte';
 	import Entry from '$components/Entry.svelte';
+	import QuickNav from '$components/QuickNav.svelte';
 </script>
 
+<QuickNav />
 <EncounterTotal />
 {#each $ff1Bestiary as entry, idx}
 	<Entry data={entry} index={idx} />
