fd925a8a58b3ddb6f6f3b6c806c4c3e7f3ada3bc
Dan Collins <dan.collins.dev@gmail.com>
2024-09-03 14:43:16 -0400

    fixed issue with dialog not reopening if closed with escape key

--- src/components/MonsterDataDisplay.svelte
+++ src/components/MonsterDataDisplay.svelte
@@ -1,32 +1,28 @@
 <script>
-	import { browser } from '$app/environment';
 	import { createEventDispatcher } from 'svelte';
 
 	const dispatch = createEventDispatcher();
 	export let opened = false;
 	export let data;
 	export let index = 0;
-    let dialog
+	let dialog;
 
-    $: if (opened === true) {
-        dialog.showModal();
-    }
-
+	$: if (opened === true) {
+		dialog.showModal();
+	}
 
 	const closeCard = () => {
-        opened = false
-        dispatch("closeModal")
-		dialog.close();
+		opened = false;
+		dispatch('closeModal', {
+			isClosed: opened
+		});
 	};
-
-    console.table(dialog)
 </script>
 
-
-<dialog bind:this={dialog} >
-    <h1>{data[index].name}</h1>
-    <div class="stats-wrapper">
-        <!-- <div class="labels">
+<dialog bind:this={dialog} open={opened} on:close={closeCard}>
+	<h1>{data[index].name}</h1>
+	<div class="stats-wrapper">
+		<!-- <div class="labels">
             <p>Name:</p>
             <p>HP:</p>    
         </div>
@@ -36,75 +32,70 @@
             <p>{data[index].HP}</p>
         </div> -->
 
+		<p>HP:</p>
+		<p class="values">{data[index].HP}</p>
 
-        <p>HP:</p>    
-        <p class="values">{data[index].HP}</p>
-        
-        <p>Attack:</p>
-        <p class="values">{data[index].attack}</p>
-        
-        <p>Defense:</p>
-        <p class="values">{data[index].defense}</p>
-        
-        <p>Accuracy:</p>
-        <p class="values">{data[index].accuracy}</p>
+		<p>Attack:</p>
+		<p class="values">{data[index].attack}</p>
 
-        <p>Agility:</p>
-        <p class="values">{data[index].agility}</p>
-        
-        <p>Intellect:</p>
-        <p class="values">{data[index].intellect}</p>
+		<p>Defense:</p>
+		<p class="values">{data[index].defense}</p>
 
-        <p>Evasion:</p>
-        <p class="values">{data[index].evasion}</p>
+		<p>Accuracy:</p>
+		<p class="values">{data[index].accuracy}</p>
 
-        <p>Magic Defense:</p>
-        <p class="values">{data[index].magicDefense}</p>
-    </div>
+		<p>Agility:</p>
+		<p class="values">{data[index].agility}</p>
 
-    <h2>Drops</h2>
+		<p>Intellect:</p>
+		<p class="values">{data[index].intellect}</p>
 
-    <div class="">
-        {#each data[index].items as item}
-            <p>{item}</p>
-        {/each}
-    </div>
+		<p>Evasion:</p>
+		<p class="values">{data[index].evasion}</p>
 
- 
-	<button  on:click={closeCard}>Close</button>
+		<p>Magic Defense:</p>
+		<p class="values">{data[index].magicDefense}</p>
+	</div>
+
+	<h2>Drops</h2>
+
+	<div class="">
+		{#each data[index].items as item}
+			<p>{item}</p>
+		{/each}
+	</div>
+
+	<button on:click={closeCard}>Close</button>
 </dialog>
 
-
 <style>
-    h1 {
-        text-align: center;
-    }
+	h1 {
+		text-align: center;
+	}
 
-    h2 {
-        text-align: center;
-    }
-    dialog {
-        /* translate: 0 -6rem; */
-    }
-    .stats-wrapper {
-        /* display: flex; */
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-template-rows: 1fr 1fr;
-        column-gap: 1rem;
-        margin: 1rem 0;
-    }
+	h2 {
+		text-align: center;
+	}
 
-    .labels {
-        text-align: left;
-        /* display: flex;
+	.stats-wrapper {
+		/* display: flex; */
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr 1fr;
+		column-gap: 1rem;
+		margin: 1rem 0;
+	}
+
+	.labels {
+		text-align: left;
+		/* display: flex;
         flex-direction: column; */
-    }
+	}
 
-    .values {
-        text-align: right;
-        /* display: flex;
+	.values {
+		text-align: right;
+		/* display: flex;
         flex-direction: column;
         justify-self: stretch; */
-    }
+	}
 </style>
--- src/routes/ff1/bestiary/+page.svelte
+++ src/routes/ff1/bestiary/+page.svelte
@@ -5,18 +5,17 @@
 	import QuickNav from '$components/QuickNav.svelte';
 	import MonsterDataDisplay from '$components/MonsterDataDisplay.svelte';
 
-	let currentMonster = 0
-	let openModal = false
+	let currentMonster = 0;
+	let openModal = false;
 
 	const updateMonsterDetails = (e) => {
-		currentMonster = e.detail.idx
-		openModal = true
-	}
+		currentMonster = e.detail.idx;
+		openModal = true;
+	};
 
 	const handleModalClose = (e) => {
-		openModal = false
-	}
-
+		openModal = e.detail.isClosed;
+	};
 </script>
 
 <QuickNav />
@@ -24,4 +23,9 @@
 {#each $ff1Bestiary as entry, idx}
 	<Entry on:showMonsterDetails={updateMonsterDetails} data={entry} index={idx} />
 {/each}
-<MonsterDataDisplay on:closeModal={handleModalClose} data={$ff1Bestiary} index={currentMonster} opened={openModal}/>
+<MonsterDataDisplay
+	on:closeModal={handleModalClose}
+	data={$ff1Bestiary}
+	index={currentMonster}
+	opened={openModal}
+/>
